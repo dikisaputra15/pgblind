acf8fb05628646d1e17995cda1db6f89bf4a02f8
Diki Saputra <diki@CNCRD.LAN>
2024-09-06 10:48:51 +0700

    update tgl now

--- app/Http/Controllers/StatistikController.php
+++ app/Http/Controllers/StatistikController.php
@@ -11,11 +11,11 @@ class StatistikController extends Controller
 {
     public function index()
     {
-        ini_set('max_execution_time', 3600);
+        // ini_set('max_execution_time', 3600);
 
         $tgl = Carbon::now();
-        // $tgl_now = $tgl->format('Y-m-d');
-        $tgl_coba = ['2024-08-29', '2024-08-31'];
+        $tgl_now = $tgl->format('Y-m-d');
+        // $tgl_coba = ['2024-08-29', '2024-08-31'];
         $icats = DB::table('wp_terms')
             ->join('wp_term_taxonomy', 'wp_terms.term_id', '=', 'wp_term_taxonomy.term_id')
             ->join('wp_term_relationships', 'wp_term_taxonomy.term_taxonomy_id', '=', 'wp_term_relationships.term_taxonomy_id')
@@ -24,8 +24,8 @@ class StatistikController extends Controller
             ->join('wp_lokasi', 'wp_w2gm_locations_relationships.location_id', '=', 'wp_lokasi.lokasi_id')
             ->select('wp_posts.ID', 'wp_posts.post_title', 'wp_w2gm_locations_relationships.id', 'wp_w2gm_locations_relationships.address_line_1', 'wp_lokasi.lokasi_name', 'wp_lokasi.province_name', 'wp_w2gm_locations_relationships.map_coords_1', 'wp_w2gm_locations_relationships.map_coords_2', 'wp_terms.name AS incident_category', 'wp_w2gm_locations_relationships.number_of_incident', 'wp_w2gm_locations_relationships.number_of_injuries', 'wp_w2gm_locations_relationships.number_of_fatalities', 'wp_w2gm_locations_relationships.additional_info', 'wp_posts.post_date', 'wp_terms.name')
             ->where('wp_posts.post_status', 'publish')
-            // ->whereDate(DB::raw('DATE(wp_posts.post_date)'), $tgl_now)
-            ->whereBetween(DB::raw('DATE(wp_posts.post_date)'), [$tgl_coba[0], $tgl_coba[1]])
+            ->whereDate(DB::raw('DATE(wp_posts.post_date)'), $tgl_now)
+            // ->whereBetween(DB::raw('DATE(wp_posts.post_date)'), [$tgl_coba[0], $tgl_coba[1]])
             ->where(function($query) {
                 $query->where('wp_terms.term_id', 2558)
                       ->orWhere('wp_terms.term_id', 2575)
